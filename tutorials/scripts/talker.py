da6811da59afdcdbf2d71a350133bbf8ba711dff
Samrat Roy <samrat.roy@kuleuven.be>
2018-02-26 15:15:06 +0100

    Added parameter support to talker
    Talker node will now accept the greeting string as a parameter.

--- tutorials/scripts/talker.py
+++ tutorials/scripts/talker.py
@@ -5,11 +5,12 @@ from std_msgs.msg import String
 
 
 def talker():
+    msg = rospy.get_param("message", "Hello world!")
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
+        hello_str = "{}".format(msg)
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
         rate.sleep()
